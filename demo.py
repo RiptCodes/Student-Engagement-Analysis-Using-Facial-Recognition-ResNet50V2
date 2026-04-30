aa6501747fa5d5864b887f36f083f7323b417739
mohamed-hafri <mohamed.hafri@hypervisionsurgical.com>
2026-04-30 08:55:47 +0100

    Revert demo webcam back to 640x480 30fps to reduce machine load

--- demo.py
+++ demo.py
@@ -178,9 +178,9 @@ def run():
         print("webcam not found")
         return
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 30)
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
     
     detector_thread = DetectionThread(face_detector, engagement_model)
